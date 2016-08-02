2f779caa15b80c3b61b0c1584fa7dd05d62f9fe4
dbb <dbb008@gmail.com>
2016-08-02 13:09:51 -0400

    Add JS

--- js/quiz-1.js
+++ js/quiz-1.js
@@ -69,8 +69,9 @@ var catStr = 'You answered the following questions incorrectly: ' + categories.j
 $("#categorylist").text(catStr);                        
 $("#categorylist").show("slow");            
 
-document.write( 'Total Incorrect:' + incorrect );
 
+$("#categorylist").text(incorrect);  
+                      
 if (cat1) { $("#category1").show("slow"); };            
 if (cat2) { $("#category2").show("slow"); };            
 if (cat3) { $("#category3").show("slow"); };            
